bd83794236fc442ca3a283ac07a654546722e884
Patrick <48148915+pat92fr@users.noreply.github.com>
2023-02-16 22:35:11 +0100

    Delete 01-RPI/linorobot2/ros2_ws/build directory

--- 01-RPI/linorobot2/ros2_ws/build/bringup/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/thales/linorobot2/ros2_ws/install/bringup'
--- 01-RPI/linorobot2/ros2_ws/build/my_robot_bringup/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/thales/linorobot2/ros2_ws/install/hoverboard-driver-pkg/share/hoverboard-driver-pkg/package.sh"
